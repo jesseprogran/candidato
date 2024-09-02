6d378b0ed84661b84e9176fc7f82c293631864b8
Jessé <jessepescador22@gmail.com>
2024-09-01 22:28:39 -0300

    realizando mais teste no javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Selecione todos os iframes
-  const iframes = document.querySelectorAll('iframe');
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Selecione todos os iframes
+//   const iframes = document.querySelectorAll('iframe');
   
-  // Remova o atributo autoplay de cada iframe
-  iframes.forEach((iframe) => {
-    const src = iframe.src;
-    iframe.src = ''; // Limpa o src temporariamente
-    iframe.src = src; // Redefine o src
+//   // Remova o atributo autoplay de cada iframe
+//   iframes.forEach((iframe) => {
+//     const src = iframe.src;
+//     iframe.src = ''; // Limpa o src temporariamente
+//     iframe.src = src; // Redefine o src
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Função para trocar a logo
+  const logoImage = document.getElementById('logoImage');
+  const originalSrc = 'src/image/logo.jpg'; // URL da logo original
+  const newSrc = 'src/image/logo-new.jpg'; // URL da nova logo
+
+  logoImage.addEventListener('click', function() {
+    if (logoImage.src === originalSrc) {
+      logoImage.src = newSrc;
+    } else {
+      logoImage.src = originalSrc;
+    }
   });
+
+  // Função para pausar todos os vídeos
+  function pauseAllVideos() {
+    const videos = document.querySelectorAll('iframe');
+    videos.forEach(video => {
+      const src = video.src;
+      video.src = ''; // Remove o src para pausar o vídeo
+      video.src = src; // Reatribui o src para o vídeo
+    });
+  }
+
+  // Chama a função para pausar todos os vídeos ao carregar a página
+  pauseAllVideos();
 });
