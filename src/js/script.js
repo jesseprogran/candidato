fc653e69ee9ca58bbac42362bd85e9a7828d1b8e
Jessé <jessepescador22@gmail.com>
2024-09-01 23:21:51 -0300

    mudando a seção video e colocando outras seção

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,9 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Selecione todos os iframes
-//   const iframes = document.querySelectorAll('iframe');
-  
-//   // Remova o atributo autoplay de cada iframe
-//   iframes.forEach((iframe) => {
-//     const src = iframe.src;
-//     iframe.src = ''; // Limpa o src temporariamente
-//     iframe.src = src; // Redefine o src
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Função para trocar a logo
-  const logoImage = document.getElementById('logoImage');
-  const originalSrc = 'src/image/logo.jpg'; // URL da logo original
-  const newSrc = 'src/image/logo-new.jpg'; // URL da nova logo
-
-  logoImage.addEventListener('click', function() {
-    if (logoImage.src === originalSrc) {
-      logoImage.src = newSrc;
-    } else {
-      logoImage.src = originalSrc;
-    }
-  });
-
-  // Função para pausar todos os vídeos
-  function pauseAllVideos() {
-    const videos = document.querySelectorAll('iframe');
-    videos.forEach(video => {
-      const src = video.src;
-      video.src = ''; // Remove o src para pausar o vídeo
-      video.src = src; // Reatribui o src para o vídeo
+  faqItems.forEach((item) => {
+    item.querySelector(".faq-question").addEventListener("click", () => {
+      item.classList.toggle("open");
     });
-  }
-
-  // Chama a função para pausar todos os vídeos ao carregar a página
-  pauseAllVideos();
+  });
 });
